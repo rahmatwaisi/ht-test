fc83a1660df1acb0d37e73353d2e8f016d320a76
Rahmat Waisi <rahmatwaisi@gmail.com>
2024-05-17 12:09:33 +0330

    HT FEAT: add tasks apis

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\Auth\AuthController;
+use App\Http\Controllers\API\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,4 +14,15 @@ Route::group(['prefix' => 'auth'], function () {
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
+});
+
+
+
+Route::middleware('auth:sanctum')->prefix('tasks')->group(function () {
+
+    Route::post('/', [TaskController::class, 'store']);
+    Route::put('/', [TaskController::class, 'bulkUpdate']);
+    Route::get('/{id}', [TaskController::class, 'show']);
+    Route::get('/', [TaskController::class, 'index']);
+
 });
