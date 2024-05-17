c5f2df72690d74ecd402fde9baaf3566e87ee134
Rahmat Waisi <rahmatwaisi@gmail.com>
2024-05-17 12:00:58 +0330

    HT FEAT: add an enum to represent and list notification channels

---
+++ app/Enums/NotificationChannel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+enum NotificationChannel: string
+{
+    case MAIL = 'mail';
+
+//case FCM = 'firebase';
+//case SMS = 'firebase';
+//case DATABASE = 'database';
+}
